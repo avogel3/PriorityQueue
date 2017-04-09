6db491d773fbc518ac66a30e1b4a35e50ab90cd8
Andrew Vogel <andrew.vogel@elvamarketing.com>
2017-04-09 16:34:42 -0400

    Add insert.

--- ruby/node.rb
+++ ruby/node.rb
@@ -1,7 +1,9 @@
 class Node
   attr_accessor :name, :priority
 
-  def initialize
+  def initialize name="__empty__", priority=-1
+    @name = name
+    @priority = priority
   end
 
 end
--- ruby/priority_queue_spec.rb
+++ ruby/priority_queue_spec.rb
@@ -1,7 +1,35 @@
 require_relative 'priority_queue'
+require_relative 'node'
+require 'pry'
 
 RSpec.describe PriorityQueue do
   it { should respond_to(:queue) }
   it { should respond_to(:size) }
 
+
+  describe "#insert" do
+    before :each do
+      @queue = PriorityQueue.new
+      @queue.insert("Walter", 21)
+    end
+
+    it "increments the size of the queue" do
+      expect(@queue.size).to eq 1
+    end
+    
+    it "adds name and priority into the queue" do
+      backing_array = @queue.queue
+      inserted_node = backing_array.first
+      expect(inserted_node.name).to eq "Walter"
+      expect(inserted_node.priority).to eq 21
+    end
+
+    it "maintains the min heap property" do
+      @queue.insert("Matthew", 13)
+      @queue.insert("Hermine", 8)
+      backing_array = @queue.queue
+      min_node = backing_array.first
+      expect(min_node.priority).to eq 8
+    end
+  end
 end
